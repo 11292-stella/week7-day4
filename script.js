bee40614b5595c75f90b227f25414466941559f0
11292-stella <stella.marucelli@gmail.com>
2025-03-20 16:08:26 +0100

    primo bottone

---
+++ script.js
@@ -0,0 +1,35 @@
+const loadimages = document.getElementById("loadImages")
+
+loadimages.addEventListener("click", () => {
+  fetch("https://api.pexels.com/v1/search?query=mountains", {
+    headers: {
+      Authorization: "wSmcKyWe1b3qNtmCLejrePP5ITTYAgIaCNfaHhr1JK3T3Gp9z3fz1FQq",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error("Errore nella richiesta API")
+      }
+    })
+    .then((data) => {
+      console.log("dati", data)
+      varieImmagini(data)
+    })
+    .catch((error) => {
+      console.log("errore", error)
+    })
+})
+
+const imageContainer = document.getElementById("imageContainer")
+
+const varieImmagini = function (data) {
+  const cards = Array.from(document.getElementsByClassName("col-md-4"))
+  cards.forEach((card, i) => {
+    const img = card.querySelector("img")
+    if (img && data.photos[i]) {
+      img.src = data.photos[i].src.medium
+    }
+  })
+}
